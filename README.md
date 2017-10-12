e05a6d3e3c374225a52e2899a5f45f5a2d438e5b
Joko Susilo <jksusilo28@gmail.com>
2017-10-12 23:22:31 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## VHG - Virtual Host Generator
+
+Create a new virtual host i.e (laravel.dev) using command line. 
+
+![ScreenShot](https://raw.github.com/jokosusilo/vhg/master/screenshot.PNG)
+
+
+### How To Use
+
+Clone this repo.
+```
+git clone https://github.com/jokosusilo/vhg.git
+```
+
+Change directory to vhg,
+And, run vhg.
+
+```
+php vhg
+```
---
+++ src/VHostGenerator.php
@@ -0,0 +1,90 @@
+<?php
+/**
+*
+*/
+class VHostGenerator
+{
+    public $vhost_template;
+    public $server_name;
+    public $document_root_path;
+    public $host_file;
+    public $apache_vhost;
+
+    function __construct($config)
+    {
+        $this->vhost_template       = $config['vhost_template'];
+        $this->server_name          = $config['server_name'];
+        $this->document_root_path   = $config['document_root_path'];
+        $this->apache_vhost         = $config['apache_vhost'];
+        $this->host_file            = $config['host_file'];
+    }
+
+    public function generate()
+    {
+        $template       = $this->readFile(dirname(__FILE__).'/'.$this->vhost_template);
+        $vhost_conf     = "\n\n".$this->replaceString($template);
+
+        // Append host config to httpd-xampp.conf
+        if ($this->appendText($this->apache_vhost, $vhost_conf)) {
+            // Append host name i.e laravel.dev
+            // to /etc/host
+            $content = "\n".'127.0.0.1 '."\t".$this->server_name;
+            if ($this->appendText($this->host_file, $content)) {
+                echo "Virtual Host created successfully\n";
+                return true;
+            }else{
+                echo "Failed to create Virtual Host\n";
+                return false;
+            }
+        }else{
+            echo "Failed to create Virtual Host\n";
+            return false;
+        }
+    }
+
+    private function readFile($file_path)
+    {
+        try {
+            $fhandle = fopen($file_path, 'r');
+            $content = fread($fhandle, filesize($file_path));
+            fclose($fhandle);
+            return $content;
+        } catch (Exception $exc) {
+            echo $exc->getMessage().'\n';
+            echo $exc->getTraceAsString();
+            return false;
+        }
+    }
+
+    private function replaceString($templateContent)
+    {
+        $templateServer         = str_replace('{{SERVER_NAME}}', $this->server_name, $templateContent);
+        $document_root_path     = $this->convertSlash($this->document_root_path);
+        $result                 = str_replace('{{DOCUMENT_ROOT}}', $document_root_path, $templateServer);
+        return $result;
+    }
+
+    private function convertSlash($document_root_path)
+    {
+        echo $document_root_path;
+        $new_path = str_replace('\\', '/', $document_root_path);
+        echo "---";
+        echo $new_path;
+        return $new_path;
+    }
+
+    private function appendText($file_path, $content)
+    {
+        try {
+            $fhandle = fopen($file_path, 'a+');
+            fwrite($fhandle, $content);
+            fclose($fhandle);
+            return true;
+        } catch (Exception $exc) {
+            echo $exc->getMessage().'\n';
+            echo $exc->getTraceAsString();
+            return false;
+        }
+    }
+
+}
